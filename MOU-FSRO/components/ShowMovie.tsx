5f556b6ba567a3cfd3914debd941015a568cfff7
fagnersro <fagnersro.dev@gmail.com>
2025-05-03 16:58:55 -0300

    ShowMovie components, modal implements

--- MOU-FSRO/components/ShowMovie.tsx
+++ MOU-FSRO/components/ShowMovie.tsx
@@ -1,6 +1,9 @@
-import { ScrollView, StyleSheet, View, Pressable, Image, ImageSourcePropType } from "react-native";
+import {Text, ScrollView, StyleSheet, View, Pressable, Image, ImageSourcePropType, Modal, Alert } from "react-native";
 
 import IMG_0086 from '../app/assets/images/png/IMG_0086.png'
+import { useState } from "react";
+import { windowHeight, windowWidth } from "@/app/assets/utils/dimensions";
+import { AntDesign } from "@expo/vector-icons";
 
 type ShowMovieType = {
   type: string;
@@ -43,31 +46,64 @@ const blockAmount: blockAmountType = [
 ]
 
 export default function ShowMovie({type}: ShowMovieType) {
-  return (
-    <ScrollView horizontal={true}>
-      {blockAmount.map((amount) => (
-        <View style={containerMovie.container} key={amount.id}>
-        <View style={containerMovie.wrapper}>
-          {blockAmount.map((amount) => (
-            <View style={containerMovie.block} key={amount.id}></View>
-          ))}
-        </View>
 
-        <View style={containerMovie.wrapperPressable}>
-          <Pressable style={containerMovie.pressabeArea} onPress={() => alert('Vídeo precisa ser programado!!')}>
-            <Image source={amount.imgMovie} style={containerMovie.pressableImage}/>
-          </Pressable>
+  const [showMovie, setShowMovie] = useState<boolean>(false)
+  
+  return (
+      <>
+        <Modal 
+          animationType="slide"
+          transparent={true}
+          visible={showMovie}
+          onRequestClose={() => {
+            setShowMovie(!showMovie);
+          }}>
+          <View style={containerPlayMovie.container}>
+            <View style={containerPlayMovie.wrapperContent}>
+              <Pressable onPress={() => setShowMovie(!showMovie)} style={containerPlayMovie.wrapperContentPressable}>
+                <AntDesign name="arrowleft" style={{color: '#fff'}} size={20}/>
+                <Text style={containerPlayMovie.text}>Voltar</Text>
+              </Pressable>
+
+              <View style={containerPlayMovie.contentMovie} >
+                <Image source={IMG_0086} style={containerPlayMovie.containerPlayMovie}/>
+              </View>
+
+              <View style={containerPlayMovie.contentText}>
+                <Text style={containerPlayMovie.textMovie}>
+                  Ao toque de "Atenção", o militar que estava à vontade em formatura retorna
+                  à posição de "descansar".
+                </Text>
+              </View>
+            </View>
+          </View>
+        </Modal>
+
+      <ScrollView horizontal={true}>
+        {blockAmount.map((amount) => (
+          <View style={containerMovie.container} key={amount.id}>
+          <View style={containerMovie.wrapper}>
+            {blockAmount.map((amount) => (
+              <View style={containerMovie.block} key={amount.id}></View>
+            ))}
+          </View>
+
+          <View style={containerMovie.wrapperPressable}>
+            <Pressable style={containerMovie.pressabeArea} onPress={() => setShowMovie((prevState) => !prevState)}>
+              <Image source={amount.imgMovie} style={containerMovie.pressableImage}/>
+            </Pressable>
+          </View>
+          
+          <View style={containerMovie.wrapper}>
+            {blockAmount.map((amount) => (
+              <View style={containerMovie.block} key={amount.id}></View>
+            ))}
+          </View>
         </View>
-        
-        <View style={containerMovie.wrapper}>
-          {blockAmount.map((amount) => (
-            <View style={containerMovie.block} key={amount.id}></View>
-          ))}
-        </View>
-      </View>
-      ))}
-    </ScrollView>
-  )
+        ))}
+        </ScrollView>
+    </>
+  );
 }
 
 const containerMovie = StyleSheet.create({
@@ -96,5 +132,56 @@ const containerMovie = StyleSheet.create({
     width: 180,
     height: 300,
     resizeMode: 'stretch',
+  }
+})
+
+const containerPlayMovie = StyleSheet.create({
+  container: {
+    width: windowWidth,
+    height: windowHeight,
+    backgroundColor: '#000000E6',
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  wrapperContent: {
+    //borderWidth: 2,
+    //borderColor: '#fff',
+    
+    padding: 20,
+    width: windowWidth,
+    height: windowHeight-150,
+  },
+  wrapperContentPressable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 3,
+  },
+  contentText: {
+    //borderWidth: 2,
+    //borderColor: '#fff',
+    height: 80,
+  },
+  contentMovie: {
+    overflow: 'hidden',
+    margin: 'auto',
+  },
+  containerPlayMovie: {
+    width: 300,
+    height: 500,
+  },
+  centrendView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: '#fff',
+  },
+  textMovie: {
+    fontSize: 15,
+    color: '#fff',
   }
 })
